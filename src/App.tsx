0b6a07224fa14d99977837a976a6c1e1382ade56
Giang <gtran527@gmail.com>
2022-08-14 12:59:38 +0700

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import { Home, Services, About, Pricing, Newsletter } from '../src/pages';
 
 function App() {
     return (
-        <div className="App">
+        <Routes>
             <NavBar />
-        </div>
+            <div className="container">
+                <Route path="/" element={<Home />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="/newsletter" element={<Newsletter />} />
+            </div>
+        </Routes>
     );
 }
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
-import { About, Home, Newsletter, Pricing, Services } from '../../pages';
 
 const NavBar = () => {
-    return (
-        <div>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/services" element={<Services />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/pricing" element={<Pricing />} />
-                <Route path="/newsletter" element={<Newsletter />} />
-            </Routes>
-        </div>
-    );
+    return <div></div>;
 };
 
 export default NavBar;
