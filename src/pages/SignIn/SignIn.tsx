f40fd39a1132d359dbe5f822cdda8ba0deb07007
Giang <gtran527@gmail.com>
2022-08-15 15:23:55 +0700

    button hide form

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,6 +1,9 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { CloseOutlined } from '@ant-design/icons';
 import './SignIn.scss';
+import NavBar from '../../components/NavBar/NavBar';
 
 type Inputs = {
     username: string;
@@ -8,31 +11,35 @@ type Inputs = {
 };
 
 const SignIn = () => {
+    const [hideForm, setHideForm] = useState(false);
     const {
         register,
         handleSubmit,
-        watch,
         formState: { errors },
     } = useForm<Inputs>();
 
-    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-
+    const onSubmit: SubmitHandler<Inputs> = (data) => data;
     return (
-        <div className="signin">
-            <h2 className="signin__title">Sign In</h2>
-            <form className="signin__form" onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor="username">Username </label>
-                <input id="username" {...register('username', { required: true, maxLength: 20 })} />
-                {errors.username && <span>Username is required</span>}
-                <label htmlFor="password">Password </label>
-                <input id="password" {...register('password', { required: true })} />
-                {errors.password && <span>Password is required</span>}
-                <button>Sign in</button>
-                <div>
-                    <span>Don't have an account</span>
-                    <Link to="signup">Sign up</Link>
+        <div className={hideForm ? 'hide' : 'wrapper'}>
+            <div className="signin">
+                <div className="signin__header">
+                    <h2 className="signin__title">Sign In</h2>
+                    <CloseOutlined onClick={() => setHideForm(true)} />
                 </div>
-            </form>
+                <form className="signin__form" onSubmit={handleSubmit(onSubmit)}>
+                    <label htmlFor="username">Username </label>
+                    <input id="username" {...register('username', { required: true, maxLength: 20 })} />
+                    {errors.username && <span>Username is required</span>}
+                    <label htmlFor="password">Password </label>
+                    <input id="password" {...register('password', { required: true })} />
+                    {errors.password && <span>Password is required</span>}
+                    <button className="signin__btn">Sign in</button>
+                    <div className="signin__footer">
+                        <span>Don't have an account?</span>
+                        <Link to="signup">Sign up</Link>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
