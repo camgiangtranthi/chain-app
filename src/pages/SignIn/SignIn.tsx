f9dff1a3a21a17554c69eb2e076e32b352cd743e
Giang <gtran527@gmail.com>
2022-08-15 11:57:35 +0700

    Signin form

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,5 +1,32 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+import './SignIn.scss';
+
+type Inputs = {
+    username: string;
+    password: string;
+};
+
 const SignIn = () => {
-    return <div className="singin">This is sign in page</div>;
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+    return (
+        <div className="signin">
+            <form className="signin__form" onSubmit={handleSubmit(onSubmit)}>
+                <input {...register('username', { required: true, maxLength: 20 })} />
+                {errors.username && <span>Username is required</span>}
+                <input {...register('password', { required: true })} />
+                {errors.password && <span>Password is required</span>}
+                <input type="submit" />
+            </form>
+        </div>
+    );
 };
 
 export default SignIn;
